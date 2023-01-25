5d5a46ddcbc3ee086c2ed272460a9a5dc5058a5c
Aissassou <91608545+Aissassou@users.noreply.github.com>
2023-01-25 15:13:50 +0100

    Revert "mock connection"
    This reverts commit 8d2a5cdbf1944fc31d23bb10c81bcbd7fae1aefb.

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,12 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import {Server} from "miragejs";
+
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
 }
-
-new Server({
-  routes() {
-    this.passthrough();
-    this.get('/login', () => require('../src/app/mock-user.data.json'));
-  }
-}) 
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
